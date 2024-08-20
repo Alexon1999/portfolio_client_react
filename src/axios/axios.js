7a5e698ccfd58697f5d6f54ec93344e24fc59f1f
Alexon1999 <alexon1999@gmail.com>
2024-08-20 02:11:34 +0200

    add sample .env

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// 'https://my-portfolio-alexon.herokuapp.com/'
+// 'https://my-portfolio-alexon.herokuapp.com'
 // local server : 'http://localhost:5000'
 const newAxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
